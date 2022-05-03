79e1cda026ac6a0ed88f618cf076f58043cb892c
Benny <benny.bellier@gmail.com>
2022-05-03 20:54:06 +0200

    update gitignore

--- src/Carcassonne.java
+++ src/Carcassonne.java
@@ -3,6 +3,5 @@ import global.Configuration;
 public class Carcassonne {
     public static void main(String[] args) throws Exception {
       Configuration.instance().logger().info("Lancement de l'application");
-
     }
 }
