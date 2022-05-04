23e99d125c9288b293161d6e8feffd1b5ebd3361
Benny <benny.bellier@gmail.com>
2022-05-04 18:40:56 +0200

    Fusion des batteries de test

--- src/Carcassonne.java
+++ src/Carcassonne.java
@@ -1,10 +1,14 @@
 import global.Configuration;
 import model.*;
+
 public class Carcassonne {
 
   public static void main(String[] args) throws Exception {
     Configuration.instance().logger().finest("Lancement de l'application");
     GameSet gameSet = new GameSet();
+    int[][] tab = new int[24][2];
+
+    Configuration.instance().logger().fine("Batterie de tests sur le plateau");
     System.out.println(gameSet.toString());
     gameSet.addTile(
       new Tile(
@@ -55,60 +59,66 @@ public class Carcassonne {
       0
     );
     System.out.println(gameSet.toString());
-    
-    int[][] tab = new int[24][2];
 
-    for (int i = 0 ; i!=24;i++){
-        switch(i){
-        
-          case 0,19,22:
-                tab[i][0]=i;
-                tab[i][1]=4;
-                break;
-
-            case 1,6,8,10,12,13:
-                tab[i][0]=i;
-                tab[i][1]=2;
-                break;
-
-            case 2:
-                tab[i][0]=2;
-                tab[i][1]=0;
-                break;
-
-            case 3,7,9,14,16,17,18:
-                tab[i][0]=i;
-                tab[i][1]=3;
-                break;
-
-            case 4,5,11,23:
-                tab[i][0]=i;
-                tab[i][1]=1;
-                break;
-
-            case 15:
-                tab[i][0]=i;
-                tab[i][1]=5;
-                break;
-        
-            case 20:
-                tab[i][0]=i;
-                tab[i][1]=8;
-                break;
-
-            case 21:
-                tab[i][0]=i;
-                tab[i][1]=9;
-                break;
-        }
-    
+    Configuration.instance().logger().fine("Batterie de tests sur la pioche");
+    for (int i = 0; i != 24; i++) {
+      switch (i) {
+        case 0:
+        case 19:
+        case 22:
+          tab[i][0] = i;
+          tab[i][1] = 4;
+          break;
+        case 1:
+        case 6:
+        case 8:
+        case 10:
+        case 12:
+        case 13:
+          tab[i][0] = i;
+          tab[i][1] = 2;
+          break;
+        case 2:
+          tab[i][0] = 2;
+          tab[i][1] = 0;
+          break;
+        case 3:
+        case 7:
+        case 9:
+        case 14:
+        case 16:
+        case 17:
+        case 18:
+          tab[i][0] = i;
+          tab[i][1] = 3;
+          break;
+        case 4:
+        case 5:
+        case 11:
+        case 23:
+          tab[i][0] = i;
+          tab[i][1] = 1;
+          break;
+        case 15:
+          tab[i][0] = i;
+          tab[i][1] = 5;
+          break;
+        case 20:
+          tab[i][0] = i;
+          tab[i][1] = 8;
+          break;
+        case 21:
+          tab[i][0] = i;
+          tab[i][1] = 9;
+          break;
+      }
     }
 
     Pioche p = new Pioche(tab);
-    for(int j = 0 ; j!=71;j++){
+    for (int j = 0; j != 71; j++) {
       int piece = p.piochePiece();
       System.out.println("piece n° : " + (j + 1));
       System.out.println("piece " + piece);
-      }
+    }
   }
 }
