5db41b06dae1b4e2aca7eeb18cd0c22f15609f67
LoubeauL <ludovic.loubeau@gmail.com>
2022-05-17 12:46:05 +0200

    mise a jour de l affichage , player background

--- src/view/Background.java
+++ src/view/Background.java
@@ -7,14 +7,49 @@ import java.io.File;
 
 public class Background extends JComponent{
 
-  Image background;
+  Image currentbackground , menu , credits , jouer , nouvellePartie , options , regles;
 
   public Background() {
     try {
-      background = ImageIO.read(new File("assets/Images/background.png"));
+      currentbackground = ImageIO.read(new File("assets/Images/background2.png"));
+      credits = ImageIO.read(new File("assets/Images/Credits.jpg"));
+      jouer = ImageIO.read(new File("assets/Images/Jouer.jpg"));
+      nouvellePartie = ImageIO.read(new File("assets/Images/NouvellePartie.jpg"));
+      options = ImageIO.read(new File("assets/Images/Options.jpg"));
+      regles = ImageIO.read(new File("assets/Images/Regles.jpg"));
     } catch (Exception e) {
       e.printStackTrace();
     }
+  }
+
+  public void affichageCredits() {
+    currentbackground = credits;
+    repaint();
+  }
+
+  public void affichageJouer() {
+    currentbackground = jouer;
+    repaint();
+  }
+
+  public void affichageNouvellePartie() {
+    currentbackground = nouvellePartie;
+    repaint();
+  }
+
+  public void affichageOptions() {
+    currentbackground = options;
+    repaint();
+  }
+
+  public void affichageRegles() {
+    currentbackground = regles;
+    repaint();
+  }
+
+  public void affichageMenu(){
+    currentbackground = menu;
+    repaint();
   }
 
   @Override
@@ -22,6 +57,6 @@ public class Background extends JComponent{
     Graphics2D drawable = (Graphics2D) g;
     int width = getSize().width;
     int height = getSize().height;
-    drawable.drawImage(background, 0, 0, width, height, null);
+    drawable.drawImage(currentbackground, 0, 0, width, height, null);
   }
 }
